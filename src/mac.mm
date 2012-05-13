cc605d9311bea7898a9370ca20928f18b83d1836
Federico "Lox" Lucignano <federico.lox@gmail.com>
2012-05-13 23:32:48 +0200

    minor cide improvements

--- src/mac.mm
+++ src/mac.mm
@@ -1,5 +1,11 @@
 #include "mac.h"
 
+inline NSWindow* getNativeWindow(MainWindow* window)
+{
+	NSView *nsview = (NSView *) window->winId();
+	return [nsview window];
+}
+
 NSString *string = [NSString string];
 // this selector is available only on OSX >= 10.7
 bool canHaveFullscreenTrigger = [string respondsToSelector:@selector(linguisticTagsInRange:scheme:options:orthography:tokenRanges:)];
@@ -8,22 +14,14 @@ void Mac::addFullscreenSwitch(MainWindow* window)
 {
 	//runtime check is needed since the app could run on OSX < 10.7
 	if (canHaveFullscreenTrigger)
-	{
-		NSView *nsview = (NSView *) window->winId();
-		NSWindow *nswindow = [nsview window];
-		[nswindow setCollectionBehavior:NSWindowCollectionBehaviorFullScreenPrimary];
-	}
+		[getNativeWindow(window) setCollectionBehavior:NSWindowCollectionBehaviorFullScreenPrimary];
 }
 
 void Mac::toggleFullscreen(MainWindow *window)
 {
 	//runtime check is needed since the app could run on OSX < 10.7
 	if (canHaveFullscreenTrigger)
-	{
-		NSView *nsview = (NSView *) window->winId();
-		NSWindow *nswindow = [nsview window];
-		[nswindow toggleFullScreen:nil];
-	}
+		[getNativeWindow(window) toggleFullScreen:nil];
 	else
 	{
 		if(window->isFullScreen())
@@ -37,13 +35,7 @@ bool Mac::isFullScreen(MainWindow *window)
 {
 	//runtime check is needed since the app could run on OSX < 10.7
 	if (canHaveFullscreenTrigger)
-	{
-		NSView *nsview = (NSView *) window->winId();
-		NSWindow *nswindow = [nsview window];
-		NSUInteger sm = [nswindow styleMask];
-
-		return ((sm & NSFullScreenWindowMask) != 0);
-	}
+		return (([getNativeWindow(window) styleMask] & NSFullScreenWindowMask) != 0);
 	else
 		return window->isFullScreen();
 }
